916b740880d3849311dba87fe0493a6f404b71c7
AR123456 <coloradoz99@gmail.com>
2021-12-19 05:32:35 -0700

    commented tinty specks

--- CSS-Paint-API-Specks/worklet.js
+++ CSS-Paint-API-Specks/worklet.js
@@ -1,30 +1,55 @@
-/// importing this way but could be NPM if using node
-////////////////////////////////// why not use Math.random()???
-//  https://css-tricks.com/creating-generative-patterns-with-the-css-paint-api/
-//All three of the patterns we are creating in this tutorial rely heavily on randomness. Paint API worklets should, however, (almost) always be deterministic. Given the same input properties and dimensions, a worklet’s paint() function should always render the same thing.
-//The Paint API may want to use a cached version of a worklet’s paint() output for better performance. Introducing an unpredictable element to a worklet renders this impossible!
-//A worklet’s paint() function re-runs whenever the element it applies to changes dimensions. When coupled with “pure” randomness, this can result in significant flashes of content — a potential accessibility issue for some folks.
-//all this renders Math.random() a little useless, as it is entirely unpredictable.
-///// Doing this so that when the browser is reszed pattern doesnt regenerate
 import random from "https://cdn.skypack.dev/random";
 import seedrandom from "https://cdn.skypack.dev/seedrandom";
-// define a worklet class with a simple paint() function:
-// When the worklet’s target element updates (changes dimensions, modifies custom properties), it re-runs. A worklet’s paint()
 
-class Worklet {
+class TinySpecksPattern {
+  // defining the getter worklet will accept the following input properties:
+  static get inputProperties() {
+    return [
+      "--pattern-seed",
+      "--pattern-colors",
+      "--pattern-speck-count",
+      "--pattern-speck-min-size",
+      "--pattern-speck-max-size",
+    ];
+  }
   paint(ctx, geometry, props) {
-    //   params
-    // ctx — a 2D drawing context very similar to that of HTML canvas
-    //geometry — an object containing the width/height dimensions of the worklet’s target element
-    //props — an array of CSS custom properties that we can “watch” for changes and re-render when they do.  way of passing values to paint worklets.
     const { width, height } = geometry;
-
-    ctx.fillStyle = "#000";
-    ctx.fillRect(0, 0, width, height);
+    // input properties vars
+    const seed = props.get("--pattern-seed").value;
+    const colors = props.getAll("--pattern-colors").map((c) => c.toString());
+    const count = props.get("--pattern-speck-count").value;
+    const minSize = props.get("--pattern-speck-min-size").value;
+    const maxSize = props.get("--pattern-speck-max-size").value;
+    // initialize our pseudo-random number generator:
+    // re-seeding seedrandom with the same seed value every time paint() runs, resulting in a consistent stream of random numbers across renders.
+    random.use(seedrandom(seed));
+    // for loop to generate each speck
+    for (let i = 0; i < count; i++) {
+      // we define an x and y position for the speck.
+      const x = random.float(0, width);
+      const y = random.float(0, height);
+      // random size for the radius
+      const radius = random.float(minSize, maxSize);
+      // random color
+      ctx.fillStyle = colors[random.int(0, colors.length - 1)];
+      // now use cts to render
+      // save the drawing contexts's state
+      ctx.save();
+      // translate the specks center point defined by x and y vars - and apply rotation
+      // translating before rotating ensures object rotates around its center axi
+      ctx.translate(x, y);
+      ctx.rotate(((random.float(0, 360) * 180) / Math.PI) * 2);
+      ctx.translate(-x, -y);
+      // draw
+      ctx.beginPath();
+      ctx.ellipse(x, y, radius, radius / 2, 0, Math.PI * 2, 0);
+      ctx.fill();
+      // restore the drawing context:
+      ctx.restore();
+    }
   }
 }
-// Once a worklet class is defined, it needs to be registered before we can use it. To do so, we call registerPaint in the worklet file itself:
-//  checking registerPaint   defined before running  JavaScript will always run once on the main browser thread — registerPaint only becomes available once the JavaScript file is loaded into a worklet using CSS.paintWorklet.addModule(...). which is in the index.html file
+
 if (typeof registerPaint !== "undefined") {
-  registerPaint("workletName", Worklet);
+  registerPaint("tinySpecksPattern", TinySpecksPattern);
 }
