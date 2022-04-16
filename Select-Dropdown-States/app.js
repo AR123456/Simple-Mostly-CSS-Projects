3edd75de3a02d77dc908f73d41352f299a5ed44d
AR123456 <coloradoz99@gmail.com>
2022-04-16 17:56:57 -0600

    import statements for states dropdown selector

--- Select-Dropdown-States/app.js
+++ Select-Dropdown-States/app.js
@@ -1,1 +1,9 @@
- 
+import Select from "./select.js";
+// added a custom data attribute to the select div in the html to select all
+const selectElements = document.querySelectorAll("[data-custom]");
+
+// instansiate to use in my project
+
+selectElements.forEach((selectElement) => {
+  console.log(new Select(selectElement));
+});
---
+++ Select-Dropdown-States/select.js
@@ -0,0 +1,9 @@
+// this is like a library I would import
+// export this class I am creating with the constructor in it
+
+export default class Select {
+  // pass in any element as  Element
+  constructor(element) {
+    this.element = element;
+  }
+}
