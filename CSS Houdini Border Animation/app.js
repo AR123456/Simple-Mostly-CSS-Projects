e284256b10b830d2053d39e099d64df5c967ddd9
AR123456 <coloradoz99@gmail.com>
2022-07-02 05:43:45 -0600

    final houdini

---
+++ CSS Houdini Border Animation/app.js
@@ -0,0 +1,19 @@
+// the non houdini way
+window.CSS.registerProperty({
+  name: "--angle",
+  syntax: "<angle>",
+  inherits: true,
+  initialValue: "90deg",
+});
+window.CSS.registerProperty({
+  name: "--gradX",
+  syntax: "<percentage>",
+  inherits: true,
+  initialValue: "50%",
+});
+window.CSS.registerProperty({
+  name: "--gradY",
+  syntax: "<percentage>",
+  inherits: true,
+  initialValue: "0%",
+});
