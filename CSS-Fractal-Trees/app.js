fc1038d0701d1408ff9925aca991e8221d600191
AR123456 <coloradoz99@gmail.com>
2023-01-01 08:15:41 -0700

    more setup fractal trees

--- CSS-Fractal-Trees/app.js
+++ CSS-Fractal-Trees/app.js
@@ -4,3 +4,9 @@ const ctx = c.getContext("2d");
 ctx.moveTo(0, 0);
 ctx.lineTo(200, 200);
 ctx.stroke();
+
+// recursion - snowflakes, crystals , tree roots, blood vessesl
+// tree of pythagoras is drawn around a right tri angel
+// Mandelbrot set
+// this is code for H-Tree fractal
+// one function with different rules
