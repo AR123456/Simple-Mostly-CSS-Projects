5fb536240e995e5124fb4b9853a135041ca7593f
AR123456 <coloradoz99@gmail.com>
2023-01-01 08:16:02 -0700

    setting up canvas

--- CSS-Fractal-Trees/app.js
+++ CSS-Fractal-Trees/app.js
@@ -1,12 +1,15 @@
-const c = document.getElementById("fractalTree");
-const ctx = c.getContext("2d");
-
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 200);
-ctx.stroke();
+const canvas = document.getElementById("fractalTree");
+const generateButton = document.querySelector(".generate-tree-button");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+// get all the methods avalable in the canvas api
+const ctx = canvas.getContext("2d");
 
 // recursion - snowflakes, crystals , tree roots, blood vessesl
 // tree of pythagoras is drawn around a right tri angel
 // Mandelbrot set
 // this is code for H-Tree fractal
 // one function with different rules
+// takes in the starting positions length of first line, angle, width branches, color of main body and color of leaves
+
+function drawTree(startX, startY, len, angel, branchWidth, color1, color2) {}
