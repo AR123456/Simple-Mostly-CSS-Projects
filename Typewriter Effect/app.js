9028fa83549bfe4142755d7aed496c17a96625b7
AR123456 <coloradoz99@gmail.com>
2026-02-13 07:26:43 -0500

    checking

--- Typewriter Effect/app.js
+++ Typewriter Effect/app.js
@@ -1,5 +1,16 @@
 // js file
-class TypeWriter extends HTMLElement{
-    console.log("in the class")
+class TypeWriter extends HTMLElement {
+  // when the browser parses html <type-writer></type-writer> , we have a new TypeWriter
+  constructor() {
+    super();
+    this._gen = 0;
+    this._paused = false;
+    this._running = false;
+    this._idx = 0;
+    this._nodes = [];
+  }
+  connectedCallback() {
+    console.log("in the html");
+  }
 }
-customElements.define("type-writer", TypeWriter)
+customElements.define("type-writer", TypeWriter);
